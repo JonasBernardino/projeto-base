618e3b4edd9b7ae0fa97a1294911db1106e8179a
jonasbernardino <jonasl.bernardino@gmail.com>
2025-10-24 16:58:26 -0300

    finalizando exemplo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./components/hero-component/hero-component";
+import { HeaderComponent } from "./components/header-component/header-component";
+import { SobreMimComponent } from "./components/sobre-mim-component/sobre-mim-component";
+import { ProjetosComponent } from "./components/projetos-component/projetos-component";
+import { ContatoComponent } from "./components/contato-component/contato-component";
+import { FooterComponent } from "./components/footer-component/footer-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, HeroComponent, SobreMimComponent, ProjetosComponent, ContatoComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('projeto-base');
 }
